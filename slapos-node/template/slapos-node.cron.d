8808de19df2431e61bae2e130ec7a4bd2c5ca879
Cédric Le Ninivin <cedric.leninivin@tiolive.com>
2012-08-23 14:51:56 +0200

    Corrected automated tasks

--- slapos-node/template/slapos-node.cron.d
+++ slapos-node/template/slapos-node.cron.d
@@ -2,7 +2,7 @@ SHELL=/bin/sh
 PATH=/usr/bin:/usr/sbin:/sbin:/bin
 MAILTO=root
 
-*/5 * * * *	root	/opt/slapos/bin/slapgrid-cp /etc/opt/slapos.cfg
-*/15 * * * *	root	/opt/slapos/bin/slapgrid-sr /etc/opt/slapos.cfg
-0   0 * * *	root	/opt/slapos/bin/slapgrid-ur /etc/opt/slapos.cfg
-0 0 * * *	root	/opt/slapos/bin/slapformat /etc/opt/slapos.cfg
+*/5 * * * *	root	/opt/slapos/bin/slapgrid-cp --verbose --log_file=/opt/slapos/slapgrid-cp.log --pidfile=/opt/slapos/slapgrid-cp.pid  /etc/opt/slapos/slapos.cfg
+*/15 * * * *	root	/opt/slapos/bin/slapgrid-sr --verbose --log_file=/opt/slapos/slapgrid-sr.log --pidfile=/opt/slapos/slapgrid-sr.pid /etc/opt/slapos/slapos.cfg
+0 0 * * *	root	/opt/slapos/bin/slapgrid-ur --verbose --log_file=/opt/slapos/slapgrid-ur.log  --pidfile=/opt/slapos/slapgrid-ur.pid /etc/opt/slapos/slapos.cfg
+0 0 * * *	root	/opt/slapos/bin/slapformat --verbose --log_file=/opt/slapos/slapformat.log /etc/opt/slapos/slapos.cfg
