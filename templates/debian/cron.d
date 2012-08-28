b8c5d52285014860c79d736f22928bfa3a6983b1
Cédric Le Ninivin <cedric.leninivin@tiolive.com>
2012-08-28 11:25:59 +0200

    Cron files are now perfect

--- templates/debian/cron.d
+++ templates/debian/cron.d
@@ -2,10 +2,10 @@ SHELL=/bin/sh
 PATH=/usr/bin:/usr/sbin:/sbin:/bin
 MAILTO=root
 
-*/5 * * * *	root	/opt/slapos/bin/slapgrid-cp --verbose --logfile=/opt/slapos/slapgrid-cp.log --pidfile=/opt/slapos/slapgrid-cp.pid  /etc/opt/slapos/slapos.cfg
-*/5 * * * *	root	/opt/slapos/bin/slapgrid-sr --verbose --logfile=/opt/slapos/slapgrid-sr.log --pidfile=/opt/slapos/slapgrid-sr.pid /etc/opt/slapos/slapos.cfg
-0 0 * * *	root	/opt/slapos/bin/slapgrid-ur --verbose --logfile=/opt/slapos/slapgrid-ur.log  --pidfile=/opt/slapos/slapgrid-ur.pid /etc/opt/slapos/slapos.cfg
-0 0 * * *	root	/opt/slapos/bin/slapformat --verbose  --log_file=/opt/slapos/slapformat.log /etc/opt/slapos/slapos.cfg
+*/5 * * * * root /opt/slapos/bin/slapgrid-sr --verbose --logfile=/opt/slapos/slapgrid-sr.log --pidfile=/opt/slapos/slapgrid-sr.pid /etc/opt/slapos/slapos.cfg >> /opt/slapos/slapgrid-sr.log 2>&1
+*/5 * * * * root /opt/slapos/bin/slapgrid-cp --verbose --logfile=/opt/slapos/slapgrid-cp.log --pidfile=/opt/slapos/slapgrid-cp.pid /etc/opt/slapos/slapos.cfg >> /opt/slapos/slapgrid-cp.log 2>&1
+0 0 * * * root /opt/slapos/bin/slapgrid-ur --verbose --logfile=/opt/slapos/slapgrid-ur.log --pidfile=/opt/slapos/slapgrid-ur.pid /etc/opt/slapos/slapos.cfg >> /opt/slapos/slapgrid-ur.log 2>&1
+0 0 * * * root /opt/slapos/bin/slapformat --verbose --log_file=/opt/slapos/slapformat.log -c /etc/opt/slapos/slapos.cfg >> /opt/slapos/slapformat.log 2>&1
 
 # XXX: SlapContainer
 */5 * * * *	root	if [ -x /opt/slapgrid/c436b64d40a48507801d06c53cc27fec/bin/slapcontainer ] ; then /opt/slapgrid/c436b64d40a48507801d06c53cc27fec/bin/slapcontainer --pid /opt/slapos/slapcontainer.pid /etc/opt/slapos/slapos.cfg /opt/slapos/slapcontainer.db > /opt/slapos/slapcontainer.log 2>&1 ; fi
