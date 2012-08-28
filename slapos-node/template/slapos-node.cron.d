c111ebbdae850fa9bbea3e70fb408a14d4bc751c
Antoine Catton <acatton@tiolive.com>
2012-08-28 11:40:36 +0900

    Add slapcontainer support on SlapOS USB Key

--- slapos-node/template/slapos-node.cron.d
+++ slapos-node/template/slapos-node.cron.d
@@ -6,3 +6,6 @@ MAILTO=root
 */5 * * * *	root	/opt/slapos/bin/slapgrid-sr --verbose --logfile=/opt/slapos/slapgrid-sr.log --pidfile=/opt/slapos/slapgrid-sr.pid /etc/opt/slapos/slapos.cfg
 0 0 * * *	root	/opt/slapos/bin/slapgrid-ur --verbose --logfile=/opt/slapos/slapgrid-ur.log  --pidfile=/opt/slapos/slapgrid-ur.pid /etc/opt/slapos/slapos.cfg
 0 0 * * *	root	/opt/slapos/bin/slapformat --verbose  --log_file=/opt/slapos/slapformat.log /etc/opt/slapos/slapos.cfg
+
+# XXX: SlapContainer
+*/5 * * * *	root	if [ -x /opt/slapgrid/c436b64d40a48507801d06c53cc27fec/bin/slapcontainer ] ; then /opt/slapgrid/c436b64d40a48507801d06c53cc27fec/bin/slapcontainer --pid /opt/slapos/slapcontainer.pid /etc/opt/slapos/slapos.cfg /opt/slapos/slapcontainer.db > /opt/slapos/slapcontainer.log 2>&1 ; fi
