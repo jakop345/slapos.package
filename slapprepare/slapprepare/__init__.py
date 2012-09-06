ec0f223f4cada2a241b7c7f93101f42aa6d4768b
Jean-Baptiste Petre <jean-baptiste.petre@nexedi.com>
2012-09-06 16:57:32 +0200

    ntp debug

--- slapprepare/slapprepare/__init__.py
+++ slapprepare/slapprepare/__init__.py
@@ -314,7 +314,7 @@ def configureNtp():
   new_ntp.write(SLAPOS_MARK)
   new_ntp.write(server+'\n')
   new_ntp.close()
-  _call(['chkconfig', '--add', 'ntp'])
+  _call(['chkconfig', 'ntp', 'on'])
   _call(['systemctl', 'enable', 'ntp.service'])
 
 
