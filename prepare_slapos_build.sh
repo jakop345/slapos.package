1436b1ab811b08024dcf12638b586386d6641e2f
Cédric de Saint Martin <cedric.dsm@tiolive.com>
2012-11-02 17:39:49 +0100

    Implement support for release numbers

--- prepare_slapos_build.sh
+++ prepare_slapos_build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -e
 
 VERSION=0.33
-RECIPE_VERSION=0.135
+RECIPE_VERSION=0.135.1
+RELEASE=1
 
+VERSION_REGEX="s/\%RECIPE_VERSION\%/$RECIPE_VERSION/g;s/\%VERSION\%/$VERSION/g;s/\%RELEASE\%/$RELEASE/g"
 CURRENT_DIRECTORY="$(pwd)"
 TEMPLATES_DIRECTORY=$CURRENT_DIRECTORY/templates
 SLAPOS_ORGINAL_DIRECTORY=slapos-node
-SLAPOS_DIRECTORY=slapos-node_$VERSION+$RECIPE_VERSION+0
+SLAPOS_DIRECTORY=slapos-node_$VERSION+$RECIPE_VERSION+$RELEASE
 OBS_DIRECTORY=$CURRENT_DIRECTORY/home:VIFIBnexedi:branches:home:VIFIBnexedi/SlapOS-Node
 
 
@@ -16,8 +18,8 @@ if [ ! -d "$CURRENT_DIRECTORY/$SLAPOS_DIRECTORY" ]; then
 fi
 
 # Prepare Makefile and offline script
-sed "s/\%RECIPE_VERSION\%/$RECIPE_VERSION/g;s/\%VERSION\%/$VERSION/g" $TEMPLATES_DIRECTORY/Makefile.in > $CURRENT_DIRECTORY/$SLAPOS_DIRECTORY/slapos/Makefile
-sed "s/\%RECIPE_VERSION\%/$RECIPE_VERSION/g;s/\%VERSION\%/$VERSION/g" $TEMPLATES_DIRECTORY/offline.sh.in > $CURRENT_DIRECTORY/$SLAPOS_DIRECTORY/slapos/offline.sh
+sed $VERSION_REGEX $TEMPLATES_DIRECTORY/Makefile.in > $CURRENT_DIRECTORY/$SLAPOS_DIRECTORY/slapos/Makefile
+sed $VERSION_REGEX $TEMPLATES_DIRECTORY/offline.sh.in > $CURRENT_DIRECTORY/$SLAPOS_DIRECTORY/slapos/offline.sh
 
 # Prepare Download Cache for SlapOS
 cd $CURRENT_DIRECTORY/$SLAPOS_DIRECTORY/slapos/
@@ -44,13 +46,13 @@ cp $CURRENT_DIRECTORY/$SLAPOS_DIRECTORY.tar.gz .
 osc add $SLAPOS_DIRECTORY.tar.gz
 
 # Prepare new specfile
-sed "s/\%RECIPE_VERSION\%/$RECIPE_VERSION/g;s/\%VERSION\%/$VERSION/g" $TEMPLATES_DIRECTORY/slapos.spec.in > slapos.spec
+sed $VERSION_REGEX $TEMPLATES_DIRECTORY/slapos.spec.in > slapos.spec
 osc add slapos.spec
 
 ##################### Prepare configuration file for .deb ############
 # Add entry to changelog
 cd $TEMPLATES_DIRECTORY/debian
-dch -pm -v $VERSION+$RECIPE_VERSION+0  --check-dirname-level=0 "New version of slapos ($VERSION+$RECIPE_VERSION)"
+dch -pm -v $VERSION+$RECIPE_VERSION+$RELEASE  --check-dirname-level=0 "New version of slapos ($VERSION+$RECIPE_VERSION+$RELEASE)"
 # Add cronfile
 cp $CURRENT_DIRECTORY/$SLAPOS_ORGINAL_DIRECTORY/template/slapos-node.cron.d $TEMPLATES_DIRECTORY/debian/cron.d
 cd $TEMPLATES_DIRECTORY
@@ -59,11 +61,11 @@ cd $OBS_DIRECTORY
 cp $TEMPLATES_DIRECTORY/debian.tar.gz .
 #prepare new .dsc file
 osc rm -f slapos*.dsc
-sed "s/\%RECIPE_VERSION\%/$RECIPE_VERSION/g;s/\%VERSION\%/$VERSION/g" $TEMPLATES_DIRECTORY/slapos.dsc.in > $SLAPOS_DIRECTORY.dsc
+sed $VERSION_REGEX $TEMPLATES_DIRECTORY/slapos.dsc.in > $SLAPOS_DIRECTORY.dsc
 osc add $SLAPOS_DIRECTORY.dsc
 
 ## Upload new Package
-osc commit -m " New SlapOS Recipe $RECIPE_VERSION"
+osc commit -m "New SlapOS Recipe $RECIPE_VERSION"
 
 # Save current version
 echo "$RECIPE_VERSION" > $CURRENT_DIRECTORY/slapos-recipe-version
