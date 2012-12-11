77d9bcd219866494d779f02c2e2ce6824e1ea7e5
Cédric de Saint Martin <cedric.dsm@tiolive.com>
2012-12-11 20:34:49 +0100

    Update slapos cron file to new entry points

--- slapos-node/template/slapos-node.cron.d
+++ slapos-node/template/slapos-node.cron.d
@@ -2,12 +2,13 @@ SHELL=/bin/sh
 PATH=/usr/bin:/usr/sbin:/sbin:/bin
 MAILTO=root
 
-* * * * * root /opt/slapos/bin/slapgrid-sr --verbose --logfile=/opt/slapos/slapgrid-sr.log --pidfile=/opt/slapos/slapgrid-sr.pid /etc/opt/slapos/slapos.cfg >> /opt/slapos/slapgrid-sr.log 2>&1
-* * * * * root /opt/slapos/bin/slapgrid-cp --verbose --logfile=/opt/slapos/slapgrid-cp.log --pidfile=/opt/slapos/slapgrid-cp.pid /etc/opt/slapos/slapos.cfg >> /opt/slapos/slapgrid-cp.log 2>&1
+* * * * * root /opt/slapos/bin/slapos node software --verbose --logfile=/opt/slapos/slapos-software.log
+* * * * * root /opt/slapos/bin/slapos node instance --verbose --logfile=/opt/slapos/slapos-instance.log
 
 # slapgrid-ur: hardcoded script to rerun slapgrid-ur if failed.
-0 * * * * root /opt/slapos/bin/slapgrid-ur --maximal_delay=3600 --verbose --logfile=/opt/slapos/slapgrid-ur.log --pidfile=/opt/slapos/slapgrid-ur.pid /etc/opt/slapos/slapos.cfg; i=20; while [ $? != 0 ]; do /opt/slapos/bin/slapgrid-ur --verbose --logfile=/opt/slapos/slapgrid-ur.log --pidfile=/opt/slapos/slapgrid-ur.pid /etc/opt/slapos/slapos.cfg >> /opt/slapos/slapgrid-ur.log 2>&1; sleep $(($i*60)); if [ $i < 20 ]; then let i++; fi; done;
+SLAPOS_REPORT_COMMAND="/opt/slapos/bin/slapos node report --maximal_delay=3600 --verbose --logfile=/opt/slapos/slapos-report.log"
+0 * * * * root $SLAPOS_REPORT_COMMAND; i=20; while [ $? != 0 ]; do SLAPOS_REPORT_COMMAND; sleep $(($i*60)); if [ $i < 20 ]; then let i++; fi; done;
 
-0 * * * * root /opt/slapos/bin/slapformat --verbose --log_file=/opt/slapos/slapformat.log -c /etc/opt/slapos/slapos.cfg >> /opt/slapos/slapformat.log 2>&1
+0 * * * * root /opt/slapos/bin/slapos node format >> /opt/slapos/slapformat.log 2>&1
 
 * * * * * root if [ -f /etc/opt/slapos/openvpn-needed  ]; then ifconfig tapVPN | grep "Scope:Global" > /dev/null ;if [ $? = 0 ]; then ROUTES=$(ip -6 r l | grep default | awk '{print $5}'); for GW in $ROUTES ; do if [ ! $GW = tapVPN ]; then /sbin/ip -6 route del default dev $GW;fi ;done ;fi ;fi
