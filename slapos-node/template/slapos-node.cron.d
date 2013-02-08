ea093c7af796960000f14512f867ed9e0993a606
Cédric de Saint Martin <cedric.dsm@tiolive.com>
2013-02-08 14:41:11 +0100

    Move logs to log directory

--- slapos-node/template/slapos-node.cron.d
+++ slapos-node/template/slapos-node.cron.d
@@ -3,14 +3,14 @@ PATH=/usr/bin:/usr/sbin:/sbin:/bin
 MAILTO=""
 
 # Run "Installation/Destruction of Software Releases" and "Deploy/Start/Stop Partitions" once per minute
-* * * * * root /opt/slapos/bin/slapos node software --verbose --logfile=/opt/slapos/slapos-node-software.log > /dev/null 2>&1
-* * * * * root /opt/slapos/bin/slapos node instance --verbose --logfile=/opt/slapos/slapos-node-instance.log > /dev/null 2>&1
+* * * * * root /opt/slapos/bin/slapos node software --verbose --logfile=/opt/slapos/log/slapos-node-software.log > /dev/null 2>&1
+* * * * * root /opt/slapos/bin/slapos node instance --verbose --logfile=/opt/slapos/log/slapos-node-instance.log > /dev/null 2>&1
 
 # Run "Destroy Partitions to be destroyed" once per hour
-0 * * * * root /opt/slapos/bin/slapos node report --maximal_delay=3600 --verbose --logfile=/opt/slapos/slapos-node-report.log > /dev/null 2>&1
+0 * * * * root /opt/slapos/bin/slapos node report --maximal_delay=3600 --verbose --logfile=/opt/slapos/log/slapos-node-report.log > /dev/null 2>&1
 
 # Run "Check/add IPs and so on" once per hour
-0 * * * * root /opt/slapos/bin/slapos node format >> /opt/slapos/slapos-node-format.log 2>&1
+0 * * * * root /opt/slapos/bin/slapos node format >> /opt/slapos/log/slapos-node-format.log 2>&1
 
 
 # Make sure we have only good network routes if we use VPN
