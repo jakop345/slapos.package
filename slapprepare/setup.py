d03aed6436da9a2e22f5700d666bbb7c5682ad31
Cédric de Saint Martin <cedric.dsm@tiolive.com>
2013-03-07 17:27:14 +0100

    Advertise development of new version.

--- slapprepare/setup.py
+++ slapprepare/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '1.0.0'
+version = '1.0.1-dev'
 name = 'slapprepare'
 long_description = open("README.txt").read() + "\n" + \
     open("CHANGES.txt").read() + "\n"
