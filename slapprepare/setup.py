eeed7ff9d85e6ea7b025f5fbdeb91c25f7f82f8c
Cédric de Saint Martin <cedric.dsm@tiolive.com>
2013-04-15 14:18:33 +0200

    Release 1.1.1.

--- slapprepare/setup.py
+++ slapprepare/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import glob
 import os
 
-version = '1.0.1-dev'
+version = '1.1.1'
 name = 'slapprepare'
 long_description = open("README.txt").read() + "\n" + \
     open("CHANGES.txt").read() + "\n"
