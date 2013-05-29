e14b3d1a652d4a4aabd1f606427e42962c999966
Jondy Zhao <jondy.zhao@gmail.com>
2013-05-30 00:10:33 +0800

    Remove all the virtual netcards installed by re6stent when uninstall slapos

--- windows/scripts/pre-uninstall.sh
+++ windows/scripts/pre-uninstall.sh
@@ -6,14 +6,26 @@
 # 
 # It will do:
 #
-#    1. Remove service cygserver
+#    1. Remove virtual netcards installed by re6stnet
 #
-#    2. Remove service syslog-ng
+#    2. Remove service cygserver and syslog-ng
 #
 if [[ ! "$(whoami)" == "Administrator" ]] ; then
     exit 1
 fi
 
+#
+# Remove virtual netcard installed by re6stnet 
+#
+for ifname in $(netsh interface show interface | gawk '{ print $3 }') ; do
+    if [[ ("$ifname" == re6stnet*) && ("$ifname" != "re6stnet-lo") ]] ; then
+        ip vpntap del dev $ifname mode true
+    fi
+done
+
+#
+# Remove services installed by cygwin
+#
 cygrunsrv.exe --remove syslog-ng
 cygrunsrv.exe --remove cygserver
 
