a9481f4bff642235c02c916be926124e5533879c
Rafael Monnerat <rafael@nexedi.com>
2013-12-11 17:13:51 -0700

    Removing files and entries from tapVPN [WIP]
    This commit should be eventually rebased and this msg edited

--- slapos-node/template/slapos-node.cron.d
+++ slapos-node/template/slapos-node.cron.d
@@ -11,7 +11,3 @@ MAILTO=""
 
 # Run "Check/add IPs and so on" once per hour
 0 * * * * root /opt/slapos/bin/slapos node format >> /opt/slapos/log/slapos-node-format.log 2>&1
-
-
-# Make sure we have only good network routes if we use VPN
-* * * * * root if [ -f /etc/opt/slapos/openvpn-needed  ]; then ifconfig tapVPN | grep "Scope:Global" > /dev/null ;if [ $? = 0 ]; then ROUTES=$(ip -6 r l | grep default | awk '{print $5}'); for GW in $ROUTES ; do if [ ! $GW = tapVPN ]; then /sbin/ip -6 route del default dev $GW > /dev/null 2>&1;fi ;done ;fi ;fi
