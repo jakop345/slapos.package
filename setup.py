f9ca5166ebb3c99bda98075c7688a075188f6e80
Rafael Monnerat <rafael@nexedi.com>
2014-06-15 06:00:51 -0300

    Version up 0.2.0 (Still under development)

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '0.1.3'
+version = '0.2.0'
 name = 'slapos.package'
 long_description = open("README.txt").read() + "\n" + \
     open("CHANGES.txt").read() + "\n"
