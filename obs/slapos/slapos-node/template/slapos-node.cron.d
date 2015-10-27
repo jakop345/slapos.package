3388278ffad15a384cd2794e81f10a84d7a5550c
Rafael Monnerat <rafael@nexedi.com>
2015-10-27 12:53:55 +0100

    Set a small maximum delay to distribute a bit the slapos queries from the clients.

--- obs/slapos/slapos-node/template/slapos-node.cron.d
+++ obs/slapos/slapos-node/template/slapos-node.cron.d
@@ -3,8 +3,8 @@ PATH=/usr/bin:/usr/sbin:/sbin:/bin
 MAILTO=""
 
 # Run "Installation/Destruction of Software Releases" and "Deploy/Start/Stop Partitions" once per minute
-* * * * * root /opt/slapos/bin/slapos node software --verbose --logfile=/opt/slapos/log/slapos-node-software.log > /dev/null 2>&1
-* * * * * root /opt/slapos/bin/slapos node instance --promise-timeout 20 --verbose --logfile=/opt/slapos/log/slapos-node-instance.log > /dev/null 2>&1
+* * * * * root /opt/slapos/bin/slapos node software --maximal_delay=30 --verbose --logfile=/opt/slapos/log/slapos-node-software.log > /dev/null 2>&1
+* * * * * root /opt/slapos/bin/slapos node instance --maximal_delay=20 --promise-timeout 20 --verbose --logfile=/opt/slapos/log/slapos-node-instance.log > /dev/null 2>&1
 
 # Run "Destroy Partitions to be destroyed" once per hour
 0 * * * * root /opt/slapos/bin/slapos node report --maximal_delay=3600 --verbose --logfile=/opt/slapos/log/slapos-node-report.log > /dev/null 2>&1
